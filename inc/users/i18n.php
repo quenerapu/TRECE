07193297deba7e91f1433453816d63b659b4589d
Iñaki Quenerapú <quenerapu@gmail.com>
2019-02-11 12:34:29 +0100

    Add files via upload

---
+++ inc/users/i18n.php
@@ -0,0 +1,154 @@
+<?php if(!defined("TRECE")):header("location:/");die();endif; ?>
+<?php
+//USERS
+
+# ..................................................
+# ..##.....##..######..########.########...######...
+# ..##.....##.##....##.##.......##.....##.##....##..
+# ..##.....##.##.......##.......##.....##.##........
+# ..##.....##..######..######...########...######...
+# ..##.....##.......##.##.......##...##.........##..
+# ..##.....##.##....##.##.......##....##..##....##..
+# ...#######...######..########.##.....##..######...
+# ..................................................
+
+// http://patorjk.com/software/taag/#p=display&f=Banner4&t=%20TRECE%20
+// http://patorjk.com/software/taag/#p=display&f=Bright&t=Deprecated
+// https://stackoverflow.com/questions/8154158/mysql-how-do-i-use-delimiters-in-triggers
+
+
+
+
+
+
+
+
+
+
+
+
+
+return [
+  "add" => [
+    "es"  => "Añadir usuario/a",
+    "gal" => "Engadir usuario/a",
+    "en"  => "Add user",
+    ],
+  "admin_list" => [
+    "es"  => "Administración de usuarios/as",
+    "gal" => "Administración de usuarios/as",
+    "en"  => "Users admin",
+    ],
+  "duplicated_email" => [
+    "es" => "Esa dirección de correo electrónico ya pertenece a otro usuario/a.",
+    "gal" => "Ese enderezo de correo electrónico xa pertence a outro usuario/a.",
+    "en" => "This eMail address is associated to a different user.",
+    ],
+  "duplicated_username" => [
+    "es" => "Otro usuario/a ya ha elegido ese nombre de usuario/a.",
+    "gal" => "Outro usuario/a xa escolleu ese nome de usuario/a.",
+    "en" => "Another user has already selected this username.",
+    ],
+  "duplicated_username_or_email" => [
+    "es" => "Nombre de usuario/a o dirección de correo electrónico repetidos.",
+    "gal" => "Nome de usuario/a ou enderezo de correo electrónico repetidos.",
+    "en" => "Duplicated username or eMail address.",
+    ],
+  "bio" => [
+    "es"  => "Bio",
+    "gal" => "Bio",
+    "en"  => "Bio",
+    ],
+  "clone" => [
+    "es"  => "Clonar usuario/a",
+    "gal" => "Clonar usuario/a",
+    "en"  => "Clone user",
+    ],
+  "edit" => [
+    "es"  => "Editar usuario/a",
+    "gal" => "Editar usuario/a",
+    "en"  => "Edit user",
+    ],
+  "female" => [
+    "es"  => "Femenino",
+    "gal" => "Feminino",
+    "en"  => "Female",
+    ],
+  "gender" => [
+    "es"  => "Género",
+    "gal" => "Xénero",
+    "en"  => "Gender",
+    ],
+  "hierarchy" => [
+    "es"  => "Jerarquía",
+    "gal" => "Xerarquía",
+    "en"  => "Hierarchy",
+    ],
+  "list" => [
+    "es"  => "Usuarios/as",
+    "gal" => "Usuarios/as",
+    "en"  => "Users",
+    ],
+  "male" => [
+    "es"  => "Masculino",
+    "gal" => "Masculino",
+    "en"  => "Male",
+    ],
+  "mandatory_field" => [
+    "es"  => "Campo obligatorio",
+    "gal" => "Campo obrigatorio",
+    "en"  => "Mandatory field",
+    ],
+  "name" => [
+    "es"  => "Nombre",
+    "gal" => "Nome",
+    "en"  => "Name",
+    ],
+  "new" => [
+    "es"  => "Nuevo usuario/a",
+    "gal" => "Novo usuario/a",
+    "en"  => "New user",
+    ],
+  "no_email" => [
+    "es"  => "No tiene eMail",
+    "gal" => "Non ten eMail",
+    "en"  => "No eMail adress",
+    ],
+  "singular" => [
+    "es"  => "Usuario/a",
+    "gal" => "Usuario/a",
+    "en"  => "User",
+    ],
+  "status" => [
+    "es"  => "Estado",
+    "gal" => "Estado",
+    "en"  => "Status",
+    ],
+  "surname" => [
+    "es"  => "Apellido/s",
+    "gal" => "Apelido/s",
+    "en"  => "Surname",
+    ],
+  "user" => [
+    "es"  => "Usuario/a",
+    "gal" => "Usuario/a",
+    "en"  => "User",
+    ],
+  "username" => [
+    "es"  => "Nombre de usuario/a",
+    "gal" => "Nome de usuario/a",
+    "en"  => "Username",
+    ],
+  "wrong_username" => [
+    "es" => "No puedes utilizar ese nombre de usuario/a.",
+    "gal" => "Non podes utilizar ese nome de usuario/a.",
+    "en" => "This username is not allowed.",
+    ],
+  "wrong_email" => [
+    "es" => "Dirección de correo electrónico incorrecta.",
+    "gal" => "Enderezo de correo electrónico incorrecto.",
+    "en" => "Wrong eMail address.",
+    ],
+  ];
+
+?>
