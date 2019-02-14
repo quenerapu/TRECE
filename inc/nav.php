0547d4e448bf436a7128a24e78da24c3a7f51527
Iñaki Quenerapú <quenerapu@gmail.com>
2019-02-14 11:58:10 +0100

    Update nav.php

--- inc/nav.php
+++ inc/nav.php
@@ -15,7 +15,7 @@
                 (file_exists($conf["dir"]["images"]."logo.png") ?
                 $conf["dir"]["images"]."logo.png" :
                 "data:image/svg+xml;base64,".str_replace("[COLOR]",$conf["logo"]["white"],$conf["logo"]["img"]))
-                ;?>" alt="<?=$conf["meta"]["title"][$conf["site"]["lang"]];?>" class="img-responsive"></a>
+                ;?>" alt="<?=$conf["meta"]["title"][LANG];?>" class="img-responsive"></a>
           </div>
           <div id="navbar" class="navbar-collapse collapse">
 <?php if ($app->getUserSignInStatus()) : ?>
@@ -23,28 +23,28 @@
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?=$app->getUserName();?> <span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                  <li><?=nav($lCommon[$conf["file"]["me"]][$conf["site"]["lang"]],$lCommon[$conf["file"]["me"]]["en"]);?></li>
+                  <li><?=nav($lCommon[$conf["file"]["me"]][LANG],$lCommon[$conf["file"]["me"]]["en"]);?></li>
                   <li role="separator" class="divider"></li>
-                  <li><a href="<?=$conf["site"]["realpathLang"]."?signout";?>"><?=$lCommon["signout"][$conf["site"]["lang"]];?></a></li>
+                  <li><a href="<?=$conf["site"]["realpathLang"]."?signout";?>"><?=$lCommon["signout"][LANG];?></a></li>
                 </ul>
               </li>
             </ul>
 <?php if ($app->getUserHierarchy() == 1) : ?>
             <ul class="nav navbar-nav navbar-right">
               <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?=$lCommon["admin"][$conf["site"]["lang"]];?> <span class="caret"></span></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?=$lCommon["admin"][LANG];?> <span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                  <li><?=nav($lCommon["gender"][$conf["site"]["lang"]],"gender");?></li>
-                  <li><?=nav($lCommon["privileges"][$conf["site"]["lang"]],"privileges");?></li>
-                  <li><?=nav($lCommon["hierarchy"][$conf["site"]["lang"]],"hierarchy");?></li>
-                  <li><?=nav($lCommon["users"][$conf["site"]["lang"]],"users");?></li>
+                  <li><?=nav($lCommon["gender"][LANG],"gender");?></li>
+                  <li><?=nav($lCommon["privileges"][LANG],"privileges");?></li>
+                  <li><?=nav($lCommon["hierarchy"][LANG],"hierarchy");?></li>
+                  <li><?=nav($lCommon["users"][LANG],"users");?></li>
                 </ul>
               </li>
             </ul>
 <?php endif; ?>
 <?php else : ?>
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="<?=$conf["site"]["realpathLang"].$conf["file"]["signin"];?>"><?=$lCommon["signin"][$conf["site"]["lang"]];?></a></li>
+              <li><a href="<?=$conf["site"]["realpathLang"].$conf["file"]["signin"];?>"><?=$lCommon["signin"][LANG];?></a></li>
             </ul>
 <?php endif; ?>
           </div>
