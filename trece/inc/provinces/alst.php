d2679f1737dab7f37abc891d3a9a14c2b817a20f
Iñaki Quenerapú <quenerapu@gmail.com>
2019-07-02 13:43:27 +0200

    Update alst.php

--- trece/inc/provinces/alst.php
+++ trece/inc/provinces/alst.php
@@ -427,7 +427,7 @@ EOD;
                   <?=$trece->parent_name[$i];?>
                 </a>
               </td>
-              <td nowrap style="text-align:right;">
+              <td style="white-space:nowrap;text-align:right;">
                 <div class="btn-group">
                   <a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?=$lCommon["actions"][LANG];?> <span class="caret"></span></a>
                   <ul class="dropdown-menu">
