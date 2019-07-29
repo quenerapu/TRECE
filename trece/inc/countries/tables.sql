f46c42e08d53ffcf3f8ca7a14b849ecd93766b63
Iñaki Quenerapú <quenerapu@gmail.com>
2019-07-29 13:05:18 +0200

    TRECE 0.15.50 (InnoDB + utf8mb4_unicode_ci)

--- trece/inc/countries/tables.sql
+++ trece/inc/countries/tables.sql
@@ -1,9 +1,7 @@
-SET NAMES utf8;
+SET NAMES utf8mb4;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
-SET NAMES utf8mb4;
 
 DROP TABLE IF EXISTS `inconceivable_countries`;
 CREATE TABLE `inconceivable_countries` (
