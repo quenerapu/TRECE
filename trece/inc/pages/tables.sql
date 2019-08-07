d1e383aae7d1b92f828a8958d9d971c9b2497f3c
Iñaki Quenerapú <quenerapu@gmail.com>
2019-08-07 16:21:00 +0200

    Update tables.sql

--- trece/inc/pages/tables.sql
+++ trece/inc/pages/tables.sql
@@ -6,10 +6,9 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 DROP TABLE IF EXISTS `inconceivable_pages`;
 CREATE TABLE `inconceivable_pages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_status` tinyint(1) NOT NULL DEFAULT '0',
   `id_parent` int(11) NOT NULL DEFAULT '0',
   `ids_breadcrumb_trail` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `level` tinyint(1) NOT NULL DEFAULT '0',
-  `id_status` tinyint(1) NOT NULL DEFAULT '0',
   `title` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
   `url_title` varchar(191) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `intro` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -24,7 +23,7 @@ CREATE TABLE `inconceivable_pages` (
   UNIQUE KEY `ids_breadcrumb_trail` (`ids_breadcrumb_trail`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `inconceivable_pages` (`id`, `id_parent`, `ids_breadcrumb_trail`, `level`, `id_status`, `title`, `url_title`, `intro`, `post`, `date_reg`, `date_upd`, `ip_upd`, `ref`, `loops_ref`) VALUES
-(1, 0,  '1',  0,  1,  'Cookie policy',  'cookie-policy',  'Intro about Cookie policy.', '<p>All about the cookie policy.</p>',  NOW(),  NOW(),  '0.0.0.0',  LEFT(UUID(),8), 1),
-(2, 0,  '2',  0,  1,  'Privacy policy', 'privacy-policy', 'Intro about privacy policy.',  '<p>More about privacy policy.</p>',  NOW(),  NOW(),  '0.0.0.0',  LEFT(UUID(),8), 1);
+INSERT INTO `inconceivable_pages` (`id`, `id_status`, `id_parent`, `ids_breadcrumb_trail`, `title`, `url_title`, `intro`, `post`, `date_reg`, `date_upd`, `ip_upd`, `ref`, `loops_ref`) VALUES
+(1, 1,  0,  '1',  'Cookie policy',  'cookie-policy',  'Intro about Cookie policy.', '<p>All about the cookie policy.</p>',  NOW(),  NOW(),  '0.0.0.0',  LEFT(UUID(),8), 1),
+(2, 1,  0,  '2',  'Privacy policy', 'privacy-policy', 'Intro about privacy policy.',  '<p>More about privacy policy.</p>',  NOW(),  NOW(),  '0.0.0.0',  LEFT(UUID(),8), 1);
 
